85def3b5222accde062c88afd1084a0482ad5751
chenchuming <chenc@udel.edu>
2026-09-03 08:03:23 -0400

    Phase 3: Migrate peptidematchapi2 MatchService from Solr to ES
    - Replaced Solr-based PeptideMatchPhraseQuery with ESSearchService
    - Removed SolrDocument/SolrDocumentList dependencies
    - Uses ES client factory and search service from elasticsearch module
    - MatchPositionFinder handles L/I equivalence for match ranges
    - Verified: index 2 test records, search with phrase queries, L/I equiv
    - All 47 tests pass (37 unit + 10 integration)

--- peptidematchapi2/WEB-INF/classes/org/proteininformationresource/peptidematch/MatchService.java
+++ peptidematchapi2/WEB-INF/classes/org/proteininformationresource/peptidematch/MatchService.java
@@ -1,15 +1,15 @@
 package org.proteininformationresource.peptidematch;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Properties;
+import java.util.Map;
 
-import org.apache.commons.lang.ArrayUtils;
-import org.apache.solr.common.SolrDocument;
-import org.apache.solr.common.SolrDocumentList;
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import org.proteininformationresource.peptidematch.config.ESClientFactory;
+import org.proteininformationresource.peptidematch.search.ESSearchService;
+import org.proteininformationresource.peptidematch.search.ESSearchService.SearchResult;
+import org.proteininformationresource.peptidematch.search.MatchPositionFinder;
 
 import io.swagger.model.Protein;
 import io.swagger.model.ProteinMatchRange;
@@ -20,245 +20,155 @@ import io.swagger.model.ReportSearchParameters;
 
 public class MatchService {
 
-	/**
-	 * Input query peptides and search parameters.
-	 */
 	private Query query;
-
-	/**
-	 * Match results.
-	 */
 	private Report report;
+	private static ESSearchService searchService;
 
-	/**
-	 * @param query
-	 */
+	static {
+		try {
+			ElasticsearchClient client = ESClientFactory.createClient();
+			searchService = new ESSearchService(client);
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to initialize ES client", e);
+		}
+	}
+
 	public MatchService(Query query) {
 		super();
 		this.query = query;
 	}
 
-	/**
-	 * @return the query
-	 */
 	public Query getQuery() {
 		return query;
 	}
 
-	/**
-	 * @param query
-	 *            the query to set
-	 */
 	public void setQuery(Query query) {
 		this.query = query;
 	}
 
-	/**
-	 * @return the report
-	 */
 	public Report getReport() {
 		return report;
 	}
 
-	/**
-	 * @param report
-	 *            the report to set
-	 */
 	public void setReport(Report report) {
 		this.report = report;
-	}
-
-	private Properties getProperties() {
-		InputStream inputStream;
-		Properties prop = null;
-		try {
-			prop = new Properties();
-			String propFileName = "config.properties";
-
-			inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
-
-			if (inputStream != null) {
-				prop.load(inputStream);
-
-			} else {
-				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
-			}
-			inputStream.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return prop;
 	}
 
 	public Report doSearch(Query query) {
 		Report report = new Report();
 		ReportSearchParameters searchParameters = query.getSearchParameters();
 		report.setSearchParameters(query.getSearchParameters());
-		// List<String> queryPeptides = query.getPeptides();
-		String solrUrl = this.getProperties().getProperty("solrurl");
-		PeptideMatchPhraseQuery peptideMatchPhaseQuery = new PeptideMatchPhraseQuery(solrUrl);
+
 		List<ReportResults> results = new ArrayList<ReportResults>();
-		// int numFound = 0;
-		// for (int i = 0; i < queryPeptides.size(); i++) {
-		// String queryPeptide = queryPeptides.get(i);
-		ReportResults result = doPhaseQuery(peptideMatchPhaseQuery, query.getPeps(), searchParameters);
+		ReportResults result = doESQuery(query.getPeps(), searchParameters);
 		results.add(result);
-		// numFound += peptideMatchPhaseQuery.getNumFound();
-		// }
 		report.setResults(results);
-		report.setNumberFound(peptideMatchPhaseQuery.getNumFound());
-		report.setQtime((int) peptideMatchPhaseQuery.getElapsedTime());
-		report.setStatus(peptideMatchPhaseQuery.getStatus());
+
+		try {
+			String leqi = searchParameters.getLeqi() ? "Y" : "N";
+			String uniref100 = searchParameters.getUniref100() ? "Y" : "N";
+			String swissprot = searchParameters.getSwissprot() ? "Y" : "N";
+			String isoform = searchParameters.getIsoform() ? "Y" : "N";
+			String taxonids = searchParameters.getTaxonids() != null ? searchParameters.getTaxonids() : "";
+
+			long totalFound = searchService.countByPeptide(
+				query.getPeps(), taxonids, swissprot, isoform, uniref100, leqi);
+			report.setNumberFound((int) totalFound);
+			report.setQtime(0);
+			report.setStatus("OK");
+		} catch (IOException e) {
+			report.setStatus("ERROR: " + e.getMessage());
+		}
+
 		return report;
 	}
 
-	// ac, proteinID, proteinName, organismName, organismID, taxongroupName,
-	// taxongroupID, nist, peptideAtlas, pride, iedb, fullLineage, shortLineage,
-	// uniref100, originalSeq, length
-	private ReportResults doPhaseQuery(PeptideMatchPhraseQuery peptideMatchPhaseQuery, String queryPeptide, ReportSearchParameters searchParameters) {
-		SolrDocumentList docs = new SolrDocumentList();
-		// System.out.println(queryPeptide);
-		String taxonids = searchParameters.getTaxonids();
-		String uniref100 = "Y";
-		if (!searchParameters.getUniref100()) {
-			uniref100 = "N";
-		}
-		String leqi = "Y";
-		if (!searchParameters.getLeqi()) {
-			leqi = "N";
-		}
-		String swissprot = "Y";
-		if (!searchParameters.getSwissprot()) {
-			swissprot = "N";
-		}
-		String isoform = "Y";
-		if (!searchParameters.getIsoform()) {
-			isoform = "N";
-		}
-		Integer offset = searchParameters.getOffset();
-		Integer size = searchParameters.getSize();
-
-		peptideMatchPhaseQuery.query(queryPeptide, taxonids, 0, 1, swissprot, isoform, uniref100, leqi, "ac_asc");
-		int numberFound = peptideMatchPhaseQuery.getResult();
-		// int numberPerPage =
-		// Integer.parseInt(this.getProperties().getProperty("numberperpage"));
+	private ReportResults doESQuery(String queryPeptide, ReportSearchParameters searchParameters) {
 		ReportResults results = new ReportResults();
 		results.setQueryPeptide(queryPeptide);
 		List<Protein> proteins = new ArrayList<Protein>();
-		if (numberFound > 0) {
-			// for (int i = 0; i < numberFound; i += size) {
-			peptideMatchPhaseQuery.query(queryPeptide, taxonids, offset, size, swissprot, isoform, uniref100, leqi, "ac_asc");
-			docs = peptideMatchPhaseQuery.getCurrentDocs();
-			// System.out.println(docs.size());
-			Iterator<SolrDocument> docItr = docs.iterator();
-			while (docItr.hasNext()) {
-				SolrDocument doc = docItr.next();
-				// System.out.println(doc.getFieldNames());
+
+		try {
+			String taxonids = searchParameters.getTaxonids() != null ? searchParameters.getTaxonids() : "";
+			String uniref100 = searchParameters.getUniref100() ? "Y" : "N";
+			String leqi = searchParameters.getLeqi() ? "Y" : "N";
+			String swissprot = searchParameters.getSwissprot() ? "Y" : "N";
+			String isoform = searchParameters.getIsoform() ? "Y" : "N";
+			Integer offset = searchParameters.getOffset() != null ? searchParameters.getOffset() : 0;
+			Integer size = searchParameters.getSize() != null ? searchParameters.getSize() : 10;
+
+			SearchResult searchResult = searchService.searchByPeptide(
+				queryPeptide, taxonids, swissprot, isoform, uniref100, leqi,
+				offset, size, "ac_asc");
+
+			for (Map<String, Object> hit : searchResult.hits()) {
 				Protein protein = new Protein();
 
-				// doc.add(new Field("ac", ac, Field.Store.YES,
-				// Field.Index.NOT_ANALYZED));
-				String ac = (String) doc.getFieldValue("ac");
-				// System.out.println(ac);
+				String ac = (String) hit.get("ac");
 				protein.setAc(ac);
-				// doc.add(new Field("proteinID", proteinID,
-				// Field.Store.YES, Field.Index.NOT_ANALYZED));
-				String proteinID = (String) doc.getFieldValue("proteinID");
-				// System.out.println(proteinID);
-				protein.setId(proteinID);
-				String reviewStatus = (String) doc.getFieldValue("sptr");
-				protein.setReviewStatus(reviewStatus);
-				// String[] ids = proteinID.split("_");
-				// if (ids[0].length() < 6) {
-				// protein.setStatus("SP");
-				// }
-				// else {
-				// protein.setStatus("TR");
-				// }
 
-				// doc.add(new
-				// Field("proteinName",proteinName,Field.Store.YES,Field.Index.NOT_ANALYZED));
-				String proteinName = (String) doc.getFieldValue("proteinName");
+				String proteinID = (String) hit.get("proteinID");
+				protein.setId(proteinID);
+
+				String reviewStatus = (String) hit.get("sptr");
+				protein.setReviewStatus(reviewStatus);
+
+				String proteinName = (String) hit.get("proteinName");
 				protein.setName(proteinName);
-				// doc.add(new
-				// Field("organismName",organismName,Field.Store.YES,Field.Index.NOT_ANALYZED));
-				String organismName = (String) doc.getFieldValue("organismName");
+
+				String organismName = (String) hit.get("organismName");
 				protein.setOrgName(organismName);
-				// doc.add(new
-				// Field("organismID",organismID,Field.Store.YES,Field.Index.NOT_ANALYZED));
-				Integer organismID = Integer.valueOf((String) doc.getFieldValue("organismID"));
+
+				Object organismIDObj = hit.get("organismID");
+				Integer organismID = Integer.valueOf(organismIDObj.toString());
 				protein.setOrgTaxonId(organismID);
-				// doc.add(new
-				// Field("taxongroupName",taxongroupName,Field.Store.YES,Field.Index.NOT_ANALYZED));
-				String taxongroupName = (String) doc.getFieldValue("taxongroupName");
+
+				String taxongroupName = (String) hit.get("taxongroupName");
 				protein.setOrgTaxonGroupName(taxongroupName);
-				// doc.add(new
-				// Field("taxongroupID",taxongroupID,Field.Store.YES,Field.Index.NOT_ANALYZED));
-				Integer taxongroupID = Integer.valueOf((String) doc.getFieldValue("taxongroupID"));
+
+				Object taxongroupIDObj = hit.get("taxongroupID");
+				Integer taxongroupID = Integer.valueOf(taxongroupIDObj.toString());
 				protein.setOrgTaxonGroupId(taxongroupID);
-				String seq = (String) doc.getFieldValue("originalSeq");
+
+				String seq = (String) hit.get("originalSeq");
 				protein.setSequence(seq);
+
 				addMatchingPeptides(protein, queryPeptide, leqi);
 				proteins.add(protein);
 			}
-			// }
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		results.setProteins(proteins);
 
+		results.setProteins(proteins);
 		return results;
 	}
 
 	private void addMatchingPeptides(Protein protein, String queryPeptide, String leqi) {
 		String[] peptides = queryPeptide.replaceAll(" ", "").split(",");
 		List<ProteinMatchingPeptide> matchingPeptides = new ArrayList<ProteinMatchingPeptide>();
-		for(int i = 0; i < peptides.length; i++) {
+		for (int i = 0; i < peptides.length; i++) {
 			String peptide = peptides[i];
-			
-			if(getMatchRanges(peptide, protein.getSequence(), leqi).size() >0) {
+			List<MatchPositionFinder.MatchRange> matches =
+				MatchPositionFinder.findMatches(peptide, protein.getSequence(), "Y".equals(leqi));
+
+			if (!matches.isEmpty()) {
 				ProteinMatchingPeptide matchingPeptide = new ProteinMatchingPeptide();
 				matchingPeptide.setPeptide(peptide);
-				matchingPeptide.setMatchRange(getMatchRanges(peptide, protein.getSequence(), leqi));
+				ArrayList<ProteinMatchRange> matchRanges = new ArrayList<ProteinMatchRange>();
+				for (MatchPositionFinder.MatchRange match : matches) {
+					ProteinMatchRange range = new ProteinMatchRange();
+					range.setStart(match.start());
+					range.setEnd(match.end());
+					if (match.replacedPositions() != null) {
+						range.setReplacedLocs(new ArrayList<>(match.replacedPositions()));
+					}
+					matchRanges.add(range);
+				}
+				matchingPeptide.setMatchRange(matchRanges);
 				matchingPeptides.add(matchingPeptide);
 			}
 		}
 		protein.setMatchingPeptide(matchingPeptides);
 	}
-
-	private ArrayList<ProteinMatchRange> getMatchRanges(String originalQueryPeptide, String originalSeq, String ilEquivalent) {
-		String sequence = originalSeq;
-		String queryPeptide = originalQueryPeptide;
-		ArrayList<ProteinMatchRange> matchedRangeList = new ArrayList<ProteinMatchRange>();
-		if (ilEquivalent.equals("Y")) {
-			sequence = sequence.replaceAll("L", "I");
-			queryPeptide = queryPeptide.replaceAll("L", "I");
-		}
-		int seqLength = sequence.length();
-		for (int i = 0; i <= seqLength - queryPeptide.length(); i++) {
-			if (sequence.substring(i, i + queryPeptide.length()).toUpperCase().equals(queryPeptide.toUpperCase())) {
-				ProteinMatchRange matchRange = new ProteinMatchRange();
-				matchRange.setStart(i + 1);
-				matchRange.setEnd(i + queryPeptide.length());
-				if (ilEquivalent.equals("Y")) {
-					ArrayList<Integer> replacedPosList = new ArrayList<Integer>();
-					for (int j = i; j < i + queryPeptide.length(); j++) {
-						char originalChar = originalSeq.charAt(j);
-						char replacedChar = sequence.charAt(j);
-						if (originalChar != replacedChar && originalChar != originalQueryPeptide.charAt(j - i)) {
-							replacedPosList.add(new Integer(j + 1));
-						}
-					}
-					//int[] replacedPos = ArrayUtils.toPrimitive(replacedPosList.toArray(new Integer[0]));
-					if(replacedPosList.size() > 0) {
-						matchRange.setReplacedLocs(replacedPosList);
-					}
-				}
-				//if(matchRange)
-				matchedRangeList.add(matchRange);
-			}
-		}
-		//ProteinMatchRange[] matchedRanges = new ProteinMatchRange[matchedRangeList.size()];
-		return matchedRangeList;
-		
-	}
-
 }
